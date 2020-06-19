40c6cb53d06d681a5f06de8401e80f7a9a8613cf
fabre_stt <afabregat92@gmail.com>
2020-06-19 13:31:14 +0100

    change config-app-rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,7 +24,7 @@ module FirstRailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
